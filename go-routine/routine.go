20672ec18cfd750a39b5f60f289f6064c5068b40
hitshiroya <hitp363530@gmail.com>
2025-07-29 01:41:12 +0530

    Completed waitgroups

--- go-routine/routine.go
+++ go-routine/routine.go
@@ -2,18 +2,29 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func printNum(i int) {
+func printNum(i int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	fmt.Println(i)
 }
 
 func main() {
 
-	for i := 0; i <= 10; i++ {
-		go printNum(i)
+	var wg sync.WaitGroup
+	for i := 0; i <= 5; i++ {
+		wg.Add(1)
+		go printNum(i, &wg)
 	}
+	wg.Wait()
 
-	time.Sleep(time.Millisecond * 3)
 }
+
+// Go lang works on GMP model.
+// G(Goroutines) - where Goroutines are lightweighted threads managed by the Go runtime.
+// M(Machine) - OS threads on which Goroutines run
+// P(Processor) - Scheduler that manages Goroutines
+
+// Go uses M:N Scheduler, where Goroutines [G] are multiplexed over OS thread [M] manage by Processor[p] and
+// his allows Go to run thousands of goroutines with very low memory and CPU overhead.
